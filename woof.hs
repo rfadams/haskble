bd278e9f9ea43dcace508a29caafd28ca73913a6
Kirubakaran Athmanathan <haskble@kirubakaran.com>
2014-05-17 13:52:03 -0700

    swap multiple times

--- woof.hs
+++ woof.hs
@@ -2,10 +2,15 @@ import System.Random
 
 swappy s n = let (a,b) = splitAt n s in concat([b,a])
 
+swapn 0 s pos = s
+swapn n s pos =
+ do
+  swapn (n-1) (swappy s pos) pos
+
 main = do
      let words = ["buttons","marlin","onomatopoeia","cough"]
-     putStrLn (swappy (words !! 0) 3)
-     putStrLn (swappy (words !! 1) 3)
-     putStrLn (swappy (words !! 2) 3)
-     putStrLn (swappy (words !! 3) 3)
-
+     -- putStrLn (swappy (words !! 0) 3)
+     -- putStrLn (swappy (words !! 1) 3)
+     -- putStrLn (swappy (words !! 2) 3)
+     -- putStrLn (swappy (words !! 3) 3)
+     putStrLn (swapn 2 "woofwoof" 3)
