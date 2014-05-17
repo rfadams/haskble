ea287165a42df471a373fc95db2e3a6f51bdac38
Robert Adams <rfadams@gmail.com>
2014-05-17 16:50:38 -0400

    it works!

--- check_input.hs
+++ check_input.hs
@@ -1,7 +1,13 @@
-main = do
+main = check_word 
+
+
+check_word = do
   let word_data = ["howdy", "owdhy"]
   putStrLn ("What's the word? " ++ (word_data !! 1))
   word <- getLine
   if word == word_data !! 0
     then putStrLn ("You win!")
-    else putStrLn ("You suck!")
+    else do 
+      putStrLn ("You suck!")
+      check_word
+
