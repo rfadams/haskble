234ae3d75a59e57f00ea1cb5e8cbbea3ac970d88
Kirubakaran Athmanathan <haskble@kirubakaran.com>
2014-05-17 13:32:27 -0700

    Get words from a list

--- woof.hs
+++ woof.hs
@@ -2,5 +2,10 @@ import System.Random
 
 swappy s n = let (a,b) = splitAt n s in concat([b,a])
 
-main = putStrLn (swappy "woofweee" 3)
+main = do
+     let words = ["buttons","marlin","onomatopoeia","cough"]
+     putStrLn (swappy (words !! 0) 3)
+     putStrLn (swappy (words !! 1) 3)
+     putStrLn (swappy (words !! 2) 3)
+     putStrLn (swappy (words !! 3) 3)
 
